11670a3d37edca2b334cc618f6f42e6b8f7c3e32
MrYy <yy1992@bupt.edu.cn>
2016-08-29 11:32:09 +0800

    添加一些注释

--- java/com/example/yy/dashgraduationdesign/Celluar/CellularSharePolicy.java
+++ java/com/example/yy/dashgraduationdesign/Celluar/CellularSharePolicy.java
@@ -6,6 +6,6 @@ import com.example.yy.dashgraduationdesign.Entities.FileFragment;
  * Created by zxc on 2016/8/29.
  */
 public interface CellularSharePolicy {
-    //蜂窝下载一个分片和如何分享策略
+    //蜂窝下载一个分片后如何分享策略
     void handleFragment(FileFragment fragment);
 }
